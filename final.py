c3f7050bd0355af34f5450dec2bf52e28ea72a98
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-05-13 16:11:56 -0700

    finished structured english

---
+++ final.py
@@ -0,0 +1,15 @@
+"""
+This program will display the number of grades, the average grade and the percentange 
+of grades that are above average grade.
+
+We will open the file Final.txt
+The code will read and copy the file then close it.
+This program will output 3 function statements, 1) number of grades, 2)average grade 
+3) percentage of grades above average
+
+A main function will be created to initialize the application.
+A function names calculate_percent_above_average will be created to calculate the 
+percentage of the average grade.
+
+
+"""
